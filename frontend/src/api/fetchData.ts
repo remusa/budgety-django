bf391ba9aeab57c91d9468081ef5dc3d6fe0601d
RMS <rene.as.himself@gmail.com>
2019-10-03 14:56:04 -0500

    Update fetchData.ts

--- frontend/src/api/fetchData.ts
+++ frontend/src/api/fetchData.ts
@@ -15,6 +15,7 @@ const getData = async (endpoint: string) => {
             method: 'GET',
         }).catch(e => {
             console.log(`ERROR: ${e.message}`)
+            return
         })
 
         if (response) {
@@ -33,6 +34,7 @@ const postData = async (endpoint: string, data: object) => {
             body: JSON.stringify(data),
         }).catch(e => {
             console.log(`ERROR: ${e.message}`)
+            return
         })
 
         if (response) {
